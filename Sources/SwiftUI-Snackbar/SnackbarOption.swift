be5a1eeb4b655c329fc055ee59cd82ef23672623
정원희 Service Development Team <wonhee.jung@creverse.com>
2023-10-04 16:32:48 +0900

    update Color Options

--- Sources/SwiftUI-Snackbar/SnackbarOption.swift
+++ Sources/SwiftUI-Snackbar/SnackbarOption.swift
@@ -13,23 +13,12 @@ public struct SnackbarOption {
     private init() {}
     
     // snackbar Color
-    var bgColor = Color(red: 37 / 255, green: 36 / 255, blue: 39 / 255)
+    public var bgColor = Color(red: 37 / 255, green: 36 / 255, blue: 39 / 255)
     
     // snackbar TextColor
-    var textColor = Color.white
+    public var textColor = Color.white
     
     // snackbar label Color
-    var labelColor = Color.blue
+    public var labelColor = Color.blue
     
-    public mutating func chnageBackgroundColor(color:Color) {
-        self.bgColor = color
-    }
-    
-    public mutating func chnageTextColor(color:Color) {
-        self.textColor = color
-    }
-    
-    public mutating func chnageLabelColor(color:Color) {
-        self.labelColor = color
-    }
 }
